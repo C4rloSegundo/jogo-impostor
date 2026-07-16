5106520d5b4888c72663cfc3879778175cf1cd3e
Carlos Segundo <carlos.segundo.dev@gmail.com>
2026-07-16 10:02:14 -0300

    feat: add service worker and manifest for PWA support

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "fake-it-cache-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/palavras.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      Promise.all(
+        ASSETS_TO_CACHE.map((url) => cache.add(url).catch(() => {}))
+      )
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(
+      (cached) =>
+        cached ||
+        fetch(event.request).catch(() => caches.match("/index.html"))
+    )
+  );
+});
